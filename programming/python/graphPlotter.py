5418313a07ce0f179c8f7121d7fa7ca5528a823d
Max Simmonds <maxsimmonds1337@gmail.com>
2022-08-22 11:28:59 +0100

    generates a single graph based on argument inputs

--- programming/python/graphPlotter.py
+++ programming/python/graphPlotter.py
@@ -1,34 +1,32 @@
 # importing the required module
+from email.base64mime import header_length
 import matplotlib.pyplot as plt
 import cmath
+import sys
 
-# get a subplot
-#plt.plot.subplots(1,4)
+x1 = float(sys.argv[1])
+x2 = float(sys.argv[2])
+y1 = float(sys.argv[3])
+y2 = float(sys.argv[4])
 
-# inital complex number
-x = 1
-y = 0
+style = sys.argv[5]
 
-origin = complex(0,0)
+# plot the data
+if style == "->":
+    width = float(sys.argv[6])
+    header_length = float(sys.argv[7])
 
-z = complex(x,y)
-i = complex(0,1)
+    plt.arrow(x1,y1,x2,y2, width = width, head_length = header_length)
+    plt.xlim([-1,1])
+    plt.ylim([-1,1])
 
-for j in range(1,5):
-    # plot the data
-    plt.subplot(1,4,j)
-    plt.plot([z])
+# naming the x axis
+plt.xlabel('x')
+# naming the y axis
+plt.ylabel('y')
 
-    # naming the x axis
-    plt.xlabel('x')
-    # naming the y axis
-    plt.ylabel('y')
-
-    # giving a title to my graph
-    plt.title('Rotating Phasor')
-
-    z = z*i ## rotate the phasor
-
+# giving a title to my graph
+plt.title('Rotating Phasor')
 
 # function to show the plot
 plt.show()
