5e7681d83fb8c2bf9a27714433b5c3973c534cec
maxsimmonds1337 <maxsimmonds1337@gmail.com>
2022-08-24 15:38:31 +0100

    testing the hook

--- programming/python/graphPlotter.py
+++ programming/python/graphPlotter.py
@@ -1,5 +1,4 @@
 # importing the required module
-from email.base64mime import header_length
 import matplotlib.pyplot as plt
 import cmath
 import sys
@@ -10,48 +9,71 @@ import subprocess
 
 result = subprocess.run(["git", "diff", "--cached", "--name-only", "--diff-filter=ACM"], stdout=subprocess.PIPE)
 
+graphCmd = []
+
 #changedFiles = os.system("git diff --cached --name-only --diff-filter=ACM")
 
 # check to see if we have anything to do
-if result.stdout != 0:
+if result.stdout.decode("utf8") != "":
     #if it's not empty, then lets get the list of paths
     paths = result.stdout.decode("utf8").split("\n")
     # now we need to file only the markdown files
+
+    changes = "" ## this will hold the new file contents, so that we can overwrite the file later
     for path in paths:
         if path[-3:] == ".md":
             #if it's a modified markdown file, then let's check for the tag
-            with open(path, 'r+') as pushedFile:
+            filename = path.split("/")
+            filename = ''.join(filename[-1:]) # get the file name with extension
+            with open(os.path.join("../../programming/python/" + filename), 'rt') as pushedFile:
                 for line in pushedFile:
                     if line.startswith("```graphPlotter"):
-                        graphCmd = next(pushedFile)
-                        break
+                        cmd = next(pushedFile)
+                        line = cmd
+                        next(pushedFile) ## this removes the ``` at the end
+                        graphCmd.append(cmd)
+                    changes = changes + line + "\n" 
+    
+    pushedFile.close()
 
-arguments = graphCmd.split(" ")
-x1 = float(arguments[0])
-y1 = float(arguments[1])
-deltaX = float(arguments[2])
-deltaY = float(arguments[3])
+    for cmd in graphCmd:
 
-style = arguments[4]
+        arguments = cmd.split(" ")
+        x1 = float(arguments[0])
+        y1 = float(arguments[1])
+        deltaX = float(arguments[2])
+        deltaY = float(arguments[3])
 
-# plot the data
-if style == "->":
-    width = float(arguments[5])
-    header_length = float(arguments[6])
+        style = arguments[4]
 
-    plt.arrow(x1,y1,deltaX,deltaY, width = width, head_length = header_length)
-    plt.xlim([-1,1])
-    plt.ylim([-1,1])
+        # plot the data
+        if style == "->":
+            width = float(arguments[5])
+            header_length = float(arguments[6])
 
-# naming the x axis
-plt.xlabel('x')
-# naming the y axis
-plt.ylabel('y')
+            plt.arrow(x1,y1,deltaX,deltaY, width = width, head_length = header_length)
+            plt.xlim([-1,1])
+            plt.ylim([-1,1])
 
-# giving a title to my graph
-plt.title('Rotating Phasor')
+        # naming the x axis
+        plt.xlabel('x')
+        # naming the y axis
+        plt.ylabel('y')
 
-# function to show the plot
-filename = uuid.uuid1().hex
-filepath = os.path.join("programming\python\images","%s.png" % filename)
-plt.savefig(filepath)
+        # giving a title to my graph
+        plt.title('Rotating Phasor')
+
+        # function to show the plot
+        imageName = uuid.uuid1().hex
+
+        cwd = os.getcwd()
+
+        filepath = os.path.join("../../programming/python/images","%s.png" % imageName)
+        plt.savefig(filepath)
+
+        link = "![Plotted with graphPlotter!](/programming/python/images/" + imageName + ".png)"  
+
+        fin = open(os.path.join("../../programming/python/" + filename), "wt")
+        changes = changes.replace(cmd, link)
+        fin.write(changes)
+        fin.close
--- programming/python/testPush.md
+++ programming/python/testPush.md
@@ -1,7 +1,9 @@
 This is a test!
 
+Changed!
+
 ```graphPlotter
 0 0 0.8 0 -> 0.02 0.1
 ```
 
-![Plotted with graphPlotter!](/programming/python/images/1c39670d223011ed999c001a7dda7113.png)
+![Plotted with graphPlotter!](/programming/python/images/1c39670d223011ed999c001a7dda7113.png)  
