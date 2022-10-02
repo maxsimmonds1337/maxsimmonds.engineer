7771768d34586f57ea721cfa80418375642b9aaa
maxsimmonds1337 <maxsimmonds1337@gmail.com>
2022-10-02 18:53:20 +0100

    updated CV

--- programming/python/test.py
+++ programming/python/test.py
@@ -1,47 +1,12 @@
-# importing the required module
-import matplotlib.pyplot as plt
-import cmath
+s = ["A"," ","m","a","n",","," ","a"," ","p","l","a","n",","," ","a"," ","c","a","n","a","l",":"," ","P","a","n","a","m","a"]
 
-# get a subplot
-#plt.plot.subplots(1,4)
-
-# inital complex number
-x = 1
-y = 0
-
-origin = complex(0,0)
-
-z = complex(x,y)
-i = complex(0,1)
-
-# plot the data
-ax = plt.subplot(1,4,1)
-plt.arrow(0,0,0.8,0, width = 0.01, head_length = 0.2)
-plt.xlim([-1,1])
-plt.ylim([-1,1])
-ax.set_aspect('equal', adjustable='box')
-
-ax = plt.subplot(1,4,2)
-plt.arrow(0,0,0,0.8, width = 0.01, head_length = 0.2)
-plt.xlim([-1,1])
-plt.ylim([-1,1])
-ax.set_aspect('equal', adjustable='box')
+length = len(s)-1
+    
+for i in range(int(length/2)):
+    ## store the var we're over writing, then swap left and right
+    tmp = s[i]
+    s[i] = s[length-i]
+    s[length-i] = tmp
 
 
-ax = plt.subplot(1,4,3)
-plt.arrow(0,0,-0.8,0, width = 0.01, head_length = 0.2)
-plt.xlim([-1,1])
-plt.ylim([-1,1])
-ax.set_aspect('equal', adjustable='box')
-
-ax = plt.subplot(1,4,4)
-plt.arrow(0,0,0,-0.8, width = 0.01, head_length = 0.2)
-plt.xlim([-1,1])
-plt.ylim([-1,1])
-ax.set_aspect('equal', adjustable='box')
-
-# giving a title to my graph
-plt.suptitle('Rotating Phasor')
-
-# function to show the plot
-plt.show()
+print("output = {0}".format(s))
