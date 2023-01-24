9490bfed15ef0b36b35cc7b78a190d6eb27bd5a9
Max Simmonds <max@maxs-air.home>
2023-01-24 21:52:27 +0000

    started writing the blogging tool:

--- Programming_Portfolio/python/blogger/make_blog.py
+++ Programming_Portfolio/python/blogger/make_blog.py
@@ -1,5 +1,25 @@
 ### code to make a new blog entry using the minimal theme for github pages
-
 from glob import glob as g
 
-print(g("./Pages/blog*.md"))
+def get_posts(blog_dir):
+    return []
+
+## print the current blogs
+
+# Get a list of dirs
+dirs = g("../../../*/")
+
+counter = 0
+for dir in dirs:
+    counter = counter + 1
+    print(f"{counter}. {dir}")
+
+blog_to_edit = int(input("Which blog would you like to add a page to?"))
+
+if blog_to_edit in range(0,counter):
+    blog_dir = dirs[blog_to_edit]
+    blog_name = blog_dir.replace("../", "")[:-1]
+    print(f"Editing blog {blog_name}...")
+    blog_posts = get_posts(blog_dir)
+else:
+    print("Blog doesn't exist!")
