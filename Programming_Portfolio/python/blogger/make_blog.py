1c75c146a684c96220626267ca890ab9a0f5b578
Max Simmonds <max@maxs-air.home>
2023-01-26 00:10:45 +0000

    wrote the get_posts function, in now returns posts

--- Programming_Portfolio/python/blogger/make_blog.py
+++ Programming_Portfolio/python/blogger/make_blog.py
@@ -1,8 +1,15 @@
 ### code to make a new blog entry using the minimal theme for github pages
 from glob import glob as g
+import re
 
 def get_posts(blog_dir):
-    return []
+    """Get's the blog posts from a given blog directory"""
+    # get the markdown files from the given directory
+    blogs =  g(blog_dir+"*.md")
+    # strip the ../ and the trailing /
+    blogs = [''.join(blog.replace("../", "")[:-1].split("/")[1:]) for blog in blogs]
+
+    return blogs
 
 ## print the current blogs
 
@@ -14,12 +21,17 @@ for dir in dirs:
     counter = counter + 1
     print(f"{counter}. {dir}")
 
-blog_to_edit = int(input("Which blog would you like to add a page to?"))
+blog_to_edit = int(input("Which blog would you like to add a page to? "))
 
 if blog_to_edit in range(0,counter):
     blog_dir = dirs[blog_to_edit]
     blog_name = blog_dir.replace("../", "")[:-1]
     print(f"Editing blog {blog_name}...")
     blog_posts = get_posts(blog_dir)
+    print(f"Current Blogs:")
+    counter = 0
+    for blog in blog_posts:
+        counter += 1
+        print(f"{counter}. {blog}")
 else:
     print("Blog doesn't exist!")
